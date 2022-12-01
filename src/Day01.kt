7930cfbee865f7212221ae9f0016b390a8aff0e0
jaavv <mjaavv@gmail.com>
2022-11-30 22:56:41 -0700

    Day 1 Solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+        var acc = 0
+        for (i in input) {
+           if (i == "")  {
+               calories.add(acc)
+               acc = 0
+           } else {
+               acc += i.toInt()
+           }
+        }
+        return calories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+        var acc = 0
+        for (i in input) {
+            if (i == "")  {
+                calories.add(acc)
+                acc = 0
+            } else {
+                acc += i.toInt()
+            }
+        }
+        var sumOfThree = 0
+        repeat(3) {
+            sumOfThree += calories.max()
+            calories.remove(calories.max())
+        }
+        return sumOfThree
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) //71023
+    println(part2(input)) //206289
 }
